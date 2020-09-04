9fad52b24a894536fa64da70c7a3a7125bab781a
Note45 <antonion.2048@gmail.com>
2020-09-04 16:56:21 -0300

    Created /transaction:POST

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,62 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: string;
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getRepository(Transaction);
+    const categoryRepository = getRepository(Category);
+
+    const checkTransactionExist = await transactionRepository.findOne({
+      where: { title },
+    });
+
+    if (checkTransactionExist) {
+      throw new AppError('Transaction already booked!');
+    }
+
+    const checkCategoryExist = await categoryRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    let categoryToBook = checkCategoryExist;
+
+    if (!checkCategoryExist) {
+      categoryToBook = categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(categoryToBook);
+    } else {
+      categoryToBook = checkCategoryExist;
+    }
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type: type === 'income' ? 'income' : 'outcome',
+      category_id: categoryToBook.id,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
