5b818ad5333b4a1cae79dcb6cb746ece0704a801
Note45 <antonion.2048@gmail.com>
2020-09-04 15:55:20 -0300

    Create models typeorm and migrations

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -8,7 +8,7 @@ import { Router } from 'express';
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  const { title, value, type, category } = request.body;
 });
 
 transactionsRouter.post('/', async (request, response) => {
